246897ac0b03d4798eec12b9696ac0a94cd41155
tomokiiiiiii <hiyokotomoki0810@icloud.com>
2022-03-30 20:12:41 +0900

    followlist css

--- resources/views/listfollowing.blade.php
+++ resources/views/listfollowing.blade.php
@@ -1,25 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-        <h1>見たい人の友達一覧</h1>
-        <!--if機能してない-->
-        @if(!isset($following_user_ids))
-        <h4>友達を追加してください</h4>
+    <h1>見たい人の友達一覧</h1>
+    <div class="split-box left-box">
         <p class='mypage'><a href="/mypage/{{Auth::id()}}">自分のページ</a></p>
-        @else
+        <p class='back'><a href='/'>日記画面</a></p>
+    </div>
+    <div class="split-box right-box">
         <div class='listfollowing'>
             @foreach($following_user_ids as $following_user_id)
-                <div class='following_user_id'>
+                <div class='followinglist'>
                     <a href="/mypage/{{ $following_user_id->id }}">{{ $following_user_id->name }}</a>
                 </div>
-                <form action="/listfollowing/{{ $following_user_id->id }}" id="form_{{ $following_user_id->id }}" method="post" style="display:inline">
-                @csrf
-                @method('DELETE')
-                <button type="submit" onClick="delete_alert(event);return false;">友達削除</button> 
-                </form>
+                <div class="delete">
+                    <form action="/listfollowing/{{ $following_user_id->id }}" id="form_{{ $following_user_id->id }}" method="post" style="display:inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" onClick="delete_alert(event);return false;">友達削除</button> 
+                    </form>
+                </div>
             @endforeach
-            <p class='mypage'><a href="/mypage/{{Auth::id()}}">自分のページ</a></p>
         </div>
-        <p class='back'><a href='/'>日記画面</a></p>
-        @endif    
+    </div>
+    
 @endsection
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.app')　　　　　　　　　　　　　　　　　
 
 @section('content')
-    <form action="/search" method="POST">
+        <form action="/search" method="POST">
             {{ csrf_field() }}
             <div class="search">
-                <h2>友達追加</h2>
+                <h1>友達追加</h1>
                 <input name="search[user_id]" placeholder="user_id"/>
                 <input name="search[name]" placeholder="name"/>
+                 <input type="submit" class="btn btn-primary" value="OK"/>
             </div>
-            <input type="submit" value="OK"/>
-            <div class='back'>[<a href='/'>日記画面</a>]</div>
+            <div class='searchback'><a href='/'>日記画面</a></div>
         </form>
-       
 
 @endsection
