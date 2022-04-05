d9861106a00ad6204ca77cd3c9d9ce48eb3adb1f
tomokiiiiiii <hiyokotomoki0810@icloud.com>
2022-04-05 11:22:47 +0900

    fileアップロードエラー解消heroku

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,7 +5,7 @@
         <h1>日記作成</h1>
         <h2>日記内容　200文字</h2>
         <form action="/diaries" method="POST"　enctype="multipart/form-data">
-            {{ csrf_field() }}
+            @csrf
             <div class="diarybox">
                 <textarea name="diary[diary]" placeholder="できごと"　onkeyup="ShowLength(value);"/>{{ old('diary.diary') }}</textarea>
                 <p id="inputlength">0文字</p>
